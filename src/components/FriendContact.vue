3e6ffffd60728daf481f1d04219b8099aa527fc8
Tonmoy Sarker <38663308+tonmoysarker@users.noreply.github.com>
2023-12-30 21:57:42 +0600

    trying out bootstrap styles

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,21 +1,27 @@
 <template>
-    <li>
-        <h2>
-            {{ friend.name }}
-            <span v-show="friend.isFavourite">(Favourite)</span>
-        </h2>
-        <button @click="toggleFavourite">Toggle Favourite</button>
-        <button @click="toggleDetails">
-            {{ detailsAreVisible ? "Hide" : "Show" }} Details
-        </button>
-        <ul v-if="detailsAreVisible">
-            <li>
-                <strong>Phone:</strong>
-                {{ friend.phone }}
+    <li class="list-group-item">
+        <div class="d-flex justify-content-between align-items-center">
+            <h2 class="mb-0">
+                {{ friend.name }}
+                <span v-if="friend.isFavourite" class="badge bg-warning ms-2"
+                    >Favourite</span
+                >
+            </h2>
+            <div>
+                <button @click="toggleFavourite" class="btn btn-secondary me-2">
+                    Toggle Favourite
+                </button>
+                <button @click="toggleDetails" class="btn btn-primary">
+                    {{ detailsAreVisible ? "Hide" : "Show" }} Details
+                </button>
+            </div>
+        </div>
+        <ul v-if="detailsAreVisible" class="list-group mt-3">
+            <li class="list-group-item">
+                <strong>Phone:</strong> {{ friend.phone }}
             </li>
-            <li>
-                <strong>Email:</strong>
-                {{ friend.email }}
+            <li class="list-group-item">
+                <strong>Email:</strong> {{ friend.email }}
             </li>
         </ul>
     </li>
